8218583898dced77f91c12a43de6311514c2fa52
RosarioRu <mariarruval@gmail.com>
2021-11-03 09:26:34 -0700

    Add function to remove all classes from body and add classe to make background dark and text white.

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $("button").click(function() {
+    $("body").removeClass();
+    $("body").addClass("dark-background");
+    $("body").addClass("light-text");
+    
+  });
+
+  
+
+});
