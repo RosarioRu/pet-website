136c91db54c6506e0c0ef5be3f880640e95967f2
RosarioRu <mariarruval@gmail.com>
2021-11-03 09:42:49 -0700

    Add function to switch back to light mode from dark.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,19 @@
 $(document).ready(function() {
-  $("button").click(function() {
+  $("#theButton").click(function() {
     $("body").removeClass();
     $("body").addClass("dark-background");
     $("body").addClass("light-text");
-    
+    $("#button2").toggle();
+    $("#theButton").toggle();
   });
 
+  $("#button2").click(function() {
+    $("body").removeClass();
+    $("#button2").toggle();
+    $("#theButton").toggle();
+    // $("body").addClass("light-background");
+    // $("#theButton").show();
+  });
   
 
 });
